4e0ff44988c99428cb3591ef33ae372dfd7becb2
Hanashiro-Shoei-20 <83803950+Hanashiro-Shoei-20@users.noreply.github.com>
2021-05-07 14:57:12 +0900

    テストです

---
+++ prob.cpp
@@ -0,0 +1,47 @@
+﻿//#include <stdio.h>
+//
+//int main() 
+//{
+//	//prob1-1
+//	printf("花城　将英\n");
+//
+//	//prob1-2
+//	printf("123\n456\n789\n");
+//
+//	//prob1-3
+//	printf("%d + %d = %d\n", 1, 1, 1 + 1);
+//	printf("%d + %d = %d\n", 2, 3, 2 + 3);
+//
+//	//prob1-4
+//	printf("%d + %d + %d = %d\n", 1, 2, 3, 1 + 2 + 3);
+//
+//	//prob2-1
+//	int a, b;
+//	printf("a = ");
+//	scanf("%d", &a);
+//	printf("b = ");
+//	scanf("%d", &b);
+//
+//	printf("a + b = %d\n", a + b);
+//	printf("a - b = %d\n", a - b);
+//	printf("a * b = %d\n", a * b);
+//	printf("a / b = %d\n", a / b);
+//	printf("a % b = %d\n", a % b);
+//
+//	//prob2-2
+//	int width = 0;
+//	int height = 0;
+//	int area = 0;
+//
+//	printf("長方形の幅：");
+//	scanf("%d", &width);
+//
+//	printf("長方形の高さ：");
+//	scanf("%d", &height);
+//
+//	area = width * height;
+//
+//	printf("長方形の面積は、%dm2です。",area);
+//
+//	return 0;
+//}
